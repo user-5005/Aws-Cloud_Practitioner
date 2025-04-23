3587acdb1c1b3e48bd4a7bac3e080b3d2828cac9
user-5005 <2022100187t@psu.palawan.edu.ph>
2025-04-23 15:58:23 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CC 6L'
+copyright = '2025, BSIT3'
+author = 'Macabingui,Alamis,Minor'
 
 release = '0.1'
 version = '0.1.0'
